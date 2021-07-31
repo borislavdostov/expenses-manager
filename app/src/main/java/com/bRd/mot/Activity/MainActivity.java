3953c96c705dd7f3d1d49f0e13fb86612210c02c
Borislav Dostov <bdostov@gmail.com>
2021-08-01 01:02:05 +0300

    Implement "Press again to exit"

--- app/src/main/java/com/bRd/mot/Activity/MainActivity.java
+++ app/src/main/java/com/bRd/mot/Activity/MainActivity.java
@@ -1,23 +1,17 @@
 package com.bRd.mot.Activity;
 
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Toast;
 
-import com.bRd.mot.Car.CarCategoryFragment;
-import com.bRd.mot.Entity.HouseCategory;
 import com.bRd.mot.Helper.ActivityDesigner;
-import com.bRd.mot.Home.HomeCategoryFragment;
-import com.bRd.mot.Home.HomeItemFragment;
-import com.bRd.mot.Job.JobFragment;
 import com.bRd.mot.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    boolean exit;
+    private static final int MILLISECONDS_TO_WAIT = 2000;
+    private long pressedTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,11 +23,13 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-            if (!exit) {
-                Toast.makeText(this, "Натиснете още веднъж за изход!", Toast.LENGTH_SHORT).show();
-                exit = true;
-            } else {
-                this.finish();
-            }
+        if (pressedTime + MILLISECONDS_TO_WAIT > System.currentTimeMillis()) {
+            super.onBackPressed();
+            finish();
+        } else {
+            Toast.makeText(getBaseContext(), "Натиснете още веднъж за изход", Toast.LENGTH_SHORT).show();
+        }
+
+        pressedTime = System.currentTimeMillis();
     }
 }
